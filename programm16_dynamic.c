14fd4d39f78fdbafafb02fb530e2beaae741e60c
DarkProtocol <svechnick@mail.ru>
2017-02-18 16:55:57 +0300

    Add files via upload

---
+++ programm16_dynamic.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define STACK_UNDERFLOW -101
+
+/*Структура стека*/
+typedef struct {
+	int * elements;
+	int number;
+} Stack;
+
+
+void stackInit(Stack * stack) {
+	stack->elements = malloc(sizeof(int));
+	stack->number = 0;
+}
+
+void push(Stack * stack, int value) {
+
+	stack->elements = realloc(stack->elements, (stack->number + 1) * sizeof(int));
+	stack->elements[stack->number] = value;
+	stack->number++;
+
+}
+
+void pop(Stack * stack)
+{	
+	if (stack->number > 0) {
+		stack->elements = realloc(stack->elements, (stack->number - 1) * sizeof(int));
+		stack->number--;
+	} else {
+		exit(STACK_UNDERFLOW);
+	}
+}
+
+/*Берет врехний стек массива*/
+int getTop(Stack * stack) {
+	if (stack->number > 0) 
+		return stack->elements[stack->number - 1];
+	else 
+		exit(STACK_UNDERFLOW);
+}
+
+/*проверяет стек на пустоту*/
+int isEmpty(Stack * stack) {
+	if (stack->number > 0) 
+		return 0;
+	else 
+		return 1;
+}
+
+/*Печатает стек*/
+void printStack(Stack * stack) {
+	if (stack->number > 0) {
+		int i = 0;
+
+		for (i = 0; i < stack->number; i++) {
+			printf("%d element = %d\n", (i + 1), stack->elements[i]);
+		}
+
+	} else {
+		printf("Stack is Empty\n");
+	}
+}
+
+int main() {
+
+	Stack *stack = (Stack*)malloc(sizeof(Stack));
+
+	stackInit(stack);
+
+	push(stack, 20);
+	push(stack, 23);
+	pop(stack);
+	printStack(stack);
+
+
+	getchar(); getchar();
+	return 0;
+}
---
+++ programm16_static.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define STACK_OVERRFLOW -101
+#define STACK_UNDERFLOW -101
+#define MAX 100
+
+/*Структура стека*/
+typedef struct {
+	int elements[MAX];
+	int number;
+} Stack;
+
+
+void stackInit(Stack * stack) {
+	stack->number = 0;
+}
+
+void push(Stack * stack, int value) {
+
+	if ( stack->number <= MAX ) {
+		stack->elements[stack->number] = value;
+		stack->number++;
+	} else {
+		exit(STACK_OVERRFLOW);
+	}
+
+}
+
+void pop(Stack * stack)
+{	
+	if (stack->number > 0) {
+		stack->number--;
+		stack->elements[stack->number] = 0;
+	} else {
+		exit(STACK_UNDERFLOW);
+	}
+}
+
+/*Берет врехний стек массива*/
+int getTop(Stack * stack) {
+	if (stack->number > 0) 
+		return stack->elements[stack->number - 1];
+	else 
+		exit(STACK_UNDERFLOW);
+}
+
+/*проверяет стек на пустоту*/
+int isEmpty(Stack * stack) {
+	if (stack->number > 0) 
+		return 0;
+	else 
+		return 1;
+}
+
+/*Печатает стек*/
+void printStack(Stack * stack) {
+	if (stack->number > 0) {
+		int i = 0;
+
+		for (i = 0; i < stack->number; i++) {
+			printf("%d element = %d\n", (i + 1), stack->elements[i]);
+		}
+
+	} else {
+		printf("Stack is Empty\n");
+	}
+}
+
+int main() {
+
+	Stack *stack = (Stack*)malloc(sizeof(Stack));
+
+	stackInit(stack);
+
+	push(stack, 20);
+	push(stack, 23);
+	pop(stack);
+	printStack(stack);
+
+
+	getchar(); getchar();
+	return 0;
+}
